0fa172b41a029e4e29b9c0db52321d024529a552
MaiQD <q.dat2710@gmail.com>
2023-06-28 15:53:04 +0700

    Assert Object type

--- 1.First Unitest/Calculations.Tests/Customer.Tests.cs
+++ 1.First Unitest/Calculations.Tests/Customer.Tests.cs
@@ -4,20 +4,28 @@ namespace Calculations.Tests
 {
     public class CustomerTest
     {
-        
-
         [Fact]
         public void CheckLegitForDiscount()
         {
             var customer = new Customer();
-            Assert.InRange(customer.Age,25,40);
+            Assert.InRange(customer.Age, 25, 40);
         }
+
         [Fact]
         public void GetOrderByNameNotNull()
         {
             var customer = new Customer();
-            var exception  = Assert.Throws<ArgumentException>(()=>customer.GetOrdersByName(null));
-            Assert.Equal("name",exception.Message);
+            var exception = Assert.Throws<ArgumentException>(() => customer.GetOrdersByName(null));
+            Assert.Equal("name", exception.Message);
+        }
+
+        [Fact]
+        public void LoyalCustomerForOrdersG100()
+        {
+            var customer = CustomerFactory.CreateCustomerInstance(102);
+            var loyalCustomer = Assert.IsType<LoyalCustomer>(customer);
+            Assert.Equal(10, loyalCustomer.Discount);
+            Assert.Equal(101, loyalCustomer.GetOrdersByName(null));
         }
     }
 }
--- 1.First Unitest/Calculations/Customer.cs
+++ 1.First Unitest/Calculations/Customer.cs
@@ -4,13 +4,36 @@
     {
         public int Age => 25;
 
-        public int GetOrdersByName(string? name)
+        public virtual int GetOrdersByName(string? name)
         {
             if (string.IsNullOrEmpty(name))
             {
                 throw new ArgumentException(nameof(name));
             }
             return 100;
+        }
+    }
+
+    public class LoyalCustomer : Customer
+    {
+        public int Discount { get; set; }
+
+        public LoyalCustomer()
+        {
+            Discount = 10;
+        }
+
+        public override int GetOrdersByName(string? name)
+        {
+            return 101;
+        }
+    }
+
+    public static class CustomerFactory
+    {
+        public static Customer CreateCustomerInstance(int orderCount)
+        {
+            return orderCount < 100 ? new Customer() : new LoyalCustomer();
         }
     }
 }
