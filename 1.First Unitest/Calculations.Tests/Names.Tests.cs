097275ba59fd92a0edf2a97678a646c5d68ed2b1
MaiQD <q.dat2710@gmail.com>
2023-06-28 15:16:32 +0700

    handle null and bool value

--- 1.First Unitest/Calculations.Tests/Names.Tests.cs
+++ 1.First Unitest/Calculations.Tests/Names.Tests.cs
@@ -11,8 +11,26 @@ namespace Calculations.Tests
             var result = names.MakeFullName("Dat", "Mai");
             Assert.Equal("Dat Mai", result, ignoreCase: true);
             Assert.Contains(" mai", result, StringComparison.InvariantCultureIgnoreCase);
-            Assert.StartsWith("Dat", result,StringComparison.CurrentCultureIgnoreCase);
+            Assert.StartsWith("Dat", result, StringComparison.CurrentCultureIgnoreCase);
             Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", result);
+        }
+
+        [Fact]
+        public void NickName_MustBeNull()
+        {
+            var names = new Names();
+            names.NickName = "Strong Man";
+            //Assert.Null(names.NickName);
+            Assert.NotNull(names.NickName);
+        }
+
+        [Fact]
+        public void MackFullName_AlwaysReturnValue()
+        {
+            var names = new Names();
+            var result = names.MakeFullName("Dat", "Mai");
+            Assert.NotNull(result);
+            Assert.False(string.IsNullOrEmpty(result));
         }
     }
 }
--- 1.First Unitest/Calculations/Names.cs
+++ 1.First Unitest/Calculations/Names.cs
@@ -2,9 +2,11 @@
 {
     public class Names
     {
+        public string NickName { get; set; }
         public string MakeFullName(string firstName, string lastName)
         {
             return $"{firstName} {lastName}";
         }
+
     }
 }
