97368645b67b85730d6be58f61536019ed0a917d
dat.mai <q.dat2710@gmail.com>
2023-08-01 23:10:25 +0700

    using Fixture xUnit

--- 1.First Unitest/Calculations.Tests/Calculations.Tests.cs
+++ 1.First Unitest/Calculations.Tests/Calculations.Tests.cs
@@ -1,27 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
+﻿using Xunit;
+using Xunit.Abstractions;
 
 namespace Calculations.Tests
 {
-    public class CalculationsTest
+    public class CalculationFixture : IDisposable
     {
+        public Calculations Calc => new();
+
+        public void Dispose()
+        {
+            //Clean
+        }
+    }
+
+    public class CalculationsTest : IClassFixture<CalculationFixture>
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+        private readonly CalculationFixture _calculationFixture;
+        private readonly MemoryStream _memoryStream;
+
+        public CalculationsTest(ITestOutputHelper testOutputHelper, CalculationFixture calculationFixture)
+        {
+            _testOutputHelper = testOutputHelper;
+            _calculationFixture = calculationFixture;
+
+            _testOutputHelper.WriteLine("Construction");
+            _memoryStream = new MemoryStream();
+        }
+
         [Fact]
-        [Trait("Category","Fibo")]
+        [Trait("Category", "Fibo")]
         public void FiboDoesNotIncludeZero()
         {
-            var calc = new Calculations();
-            Assert.All(calc.FiboNumbers, n => Assert.NotEqual(0,n));
+            _testOutputHelper.WriteLine("FiboDoesNotIncludeZero");
+            var calc = _calculationFixture.Calc;
+            Assert.All(calc.FiboNumbers, n => Assert.NotEqual(0, n));
         }
 
         [Fact]
         [Trait("Category", "Fibo")]
         public void FiboInclude13()
         {
-            var calc = new Calculations();
+            _testOutputHelper.WriteLine("FiboInclude13");
+            var calc = _calculationFixture.Calc;
             Assert.Contains(13, calc.FiboNumbers);
         }
 
@@ -29,7 +50,8 @@ namespace Calculations.Tests
         [Trait("Category", "Fibo")]
         public void FiboDoesNotInclude4()
         {
-            var calc = new Calculations();
+            _testOutputHelper.WriteLine("FiboDoesNotInclude4");
+            var calc = _calculationFixture.Calc;
             Assert.DoesNotContain(4, calc.FiboNumbers);
         }
 
@@ -37,8 +59,9 @@ namespace Calculations.Tests
         [Trait("Category", "Fibo")]
         public void Fibo_CheckCollection()
         {
-            var expetedCollection = new List<int> {1, 1, 2, 3, 5, 8, 13, 21};
-            var calc = new Calculations();
+            _testOutputHelper.WriteLine("Fibo_CheckCollection");
+            var expetedCollection = new List<int> { 1, 1, 2, 3, 5, 8, 13, 21 };
+            var calc = _calculationFixture.Calc;
             Assert.Equal(expetedCollection, calc.FiboNumbers);
         }
     }
